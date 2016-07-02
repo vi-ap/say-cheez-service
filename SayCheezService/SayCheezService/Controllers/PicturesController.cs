077a3913cf9c930bbbd81adc535ced7da8c7d424
vi-ap <aparna.23@outlook.com>
2016-07-02 16:11:23 -0700

    Adding a model and controller for picture.

---
+++ SayCheezService/SayCheezService/Controllers/PicturesController.cs
@@ -0,0 +1,61 @@
+﻿using SayCheezService.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace SayCheezService.Controllers
+{
+    public class PicturesController : ApiController
+    {
+        // GET: api/Pictures
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Pictures/5
+        public Picture Get(int id)
+        {
+            return new Picture
+            {
+                time = DateTime.Now,
+                photo = getMinionPicture()
+            };
+        }
+
+        // POST: api/Pictures
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Pictures/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Pictures/5
+        public void Delete(int id)
+        {
+        }
+
+        private byte[] getMinionPicture()
+        {
+            Debug.WriteLine(HttpContext.Current.Server.MapPath("~/Content/minion-hitman.png"));
+            FileStream stream = new FileStream(HttpContext.Current.Server.MapPath("~/Content/minion-hitman.png"), FileMode.Open, FileAccess.Read);
+            BinaryReader reader = new BinaryReader(stream);
+
+            byte[] photo = reader.ReadBytes((int)stream.Length);
+
+            reader.Close();
+            stream.Close();
+
+            return photo;
+        }
+    }
+}
---
+++ SayCheezService/SayCheezService/Models/Picture.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SayCheezService.Models
+{
+    public class Picture
+    {
+        public DateTime time { get; set; }
+        public byte[] photo { get; set; }
+    }
+}
