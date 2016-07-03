fd0f349a78ef6e09118e518a79079f370761724d
vi-ap <aparna.23@outlook.com>
2016-07-03 16:06:49 -0700

    Changing controllers to reflect changes in pictures model

--- SayCheezService/Controllers/PicturesController.cs
+++ SayCheezService/Controllers/PicturesController.cs
@@ -46,7 +46,7 @@ namespace SayCheezService.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Time,Content")] Picture picture)
+        public ActionResult Create([Bind(Include = "Id,Time,Content,SerializedContent")] Picture picture)
         {
             if (ModelState.IsValid)
             {
@@ -78,7 +78,7 @@ namespace SayCheezService.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Time,Content")] Picture picture)
+        public ActionResult Edit([Bind(Include = "Id,Time,Content,SerializedContent")] Picture picture)
         {
             if (ModelState.IsValid)
             {
